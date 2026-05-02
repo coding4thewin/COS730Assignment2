6d311ac12570fd67e30add2c104e640bd341c43c
coding4thewin <coding4thewin@gmail.com>
2026-05-02 14:58:07 +0200

    Add project files.

---
+++ UnoptimizedSystem/Classes/SubmissionModel.cs
@@ -0,0 +1,7 @@
+﻿namespace UnoptimizedSystem.Classes
+{
+    public class SubmissionModel
+    {
+
+    }
+}
---
+++ UnoptimizedSystem/Controllers/SubmissionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UnoptimizedSystem.Classes;
+
+namespace UnoptimizedSystem.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SubmissionController : Controller
+    {
+        [HttpGet]
+        public string Index() 
+        {
+            return "Hello, World!";
+        }
+
+        [HttpPost("submit")]
+        public string Submit([FromForm] SubmissionModel submission)
+        {
+            Console.WriteLine("submission:");
+            Console.WriteLine(submission);
+            return "Submission received";
+        }
+    }
+}
